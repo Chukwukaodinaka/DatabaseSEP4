f4df9f0fc5e758d0151a2289825b0dbad4d24ea8
Mazils <arturasmaziliauskas2@gmail.com>
2020-11-12 19:47:57 +0100

    Connection to the cloud hosted DB is added

--- src/main/java/org/SEP4_Data/android_endpoint/measurement/MeasurementController.java
+++ src/main/java/org/SEP4_Data/android_endpoint/measurement/MeasurementController.java
@@ -2,10 +2,14 @@ package org.SEP4_Data.android_endpoint.measurement;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.SEP4_Data.iot_gateway_endpoint.MeasurementDTO;
 import org.SEP4_Data.service.MeasurementService;
 import org.SEP4_Data.service.model.Measurements;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,16 +19,16 @@ public class MeasurementController {
     private MeasurementService service;
 
     @GetMapping("/current")
-    public Measurements getMeasurement(){/*
-        MeasurementDTO currentM= new MeasurementDTO();
-        currentM.setId(1);
-        currentM.setId_device(001);
-        currentM.setDate(LocalDate.of(2020, 05,01));
-        currentM.setTime(LocalTime.of(7,15));
-        currentM.setLight(true);
-        currentM.setTemperature(28);
-        currentM.setHumidity(100);
-        currentM.setCo2(67);*/
+    public Measurements getMeasurement(){
+//        MeasurementDTO currentM= new MeasurementDTO();
+//        currentM.setId(1);
+//        currentM.setId_device(001);
+//        currentM.setDate(LocalDate.of(2020, 05,01));
+//        currentM.setTime(LocalTime.of(7,15));
+//        currentM.setLight(true);
+//        currentM.setTemperature(28);
+//        currentM.setHumidity(100);
+//        currentM.setCo2(67);
 
         Measurements currentM = service.getCurrentMeasurements();
 
