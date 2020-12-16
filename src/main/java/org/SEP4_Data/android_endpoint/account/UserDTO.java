c3565e2321373348c753343999bca1d357d40c9d
Rafal Pierscieniak <279471@via.dk>
2020-12-16 14:51:53 +0100

    no arg constructor in dto

--- src/main/java/org/SEP4_Data/android_endpoint/account/UserDTO.java
+++ src/main/java/org/SEP4_Data/android_endpoint/account/UserDTO.java
@@ -1,13 +1,13 @@
 package org.SEP4_Data.android_endpoint.account;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Data
 public class UserDTO {
 
-    @NonNull
     private String username;
 
-    @NonNull private String password;
+    private String password;
 }
