4edfddd057cd8e2886b1bc9d3340d149b6774ae1
Mazils <arturasmaziliauskas2@gmail.com>
2020-12-16 19:55:21 +0100

    time fixes in progress

--- src/main/java/org/SEP4_Data/iot_gateway_endpoint/PayLoadDTO.java
+++ src/main/java/org/SEP4_Data/iot_gateway_endpoint/PayLoadDTO.java
@@ -31,4 +31,5 @@ public class PayLoadDTO {
     private String data;
     private String time;
 }
+
 //{​​"cmd":"rx","seqno":1102,"EUI":"0004A30B00259D2C","ts":1608138185016,"fcnt":0,"port":1,"freq":867100000,"rssi":-111,"snr":1,"toa":0,"dr":"SF12 BW125 4/5","ack":false,"bat":255,"offline":false,"data":"00000000000000000000000000000000"}​​
